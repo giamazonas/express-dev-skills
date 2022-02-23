d8fd7dd13384ac4349c3518760a07083d0b6cfdd
giamazonas <skibinskirenee@gmail.com>
2022-02-23 18:16:24 -0500

    skills add

---
+++ data/skills-db.js
@@ -0,0 +1,35 @@
+const skills = [
+  {text: 'invisibility', hasSkill: true, _id: 202920},
+  {text: 'flying', hasSkill: false, _id: 202999},
+  {text: 'time travel', hasSkill: true, _id: 202981},
+  {text: 'controlled regeneration', hasSkill: false, _id: 202902}
+]
+
+const find = (conditions, callback) => {
+  try{
+    if (!(conditions instanceof Object)){
+      throw new TypeError('Please pass in an object')
+    }
+    if (Object.keys(conditions).length === 0) return callback 
+    (null, skills)
+  }catch (error) {
+    console.log(error)
+    callback(error, [])
+  }
+}
+
+const findById = (id, callback) =>{
+  try {
+    const skill = skills.find(skill => skill._id === parseInt(id))
+    if (!skill) throw new Error ('No skill was found')
+    return callback(null, skill)
+  } catch (error) {
+    console.log(error)
+    return callback(error, null)
+  }
+}
+
+export {
+  find, 
+  findById
+}
