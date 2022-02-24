02e1722080f5d71628a613fbb5ffb8d970f78716
giamazonas <skibinskirenee@gmail.com>
2022-02-23 20:22:23 -0500

    view file add

--- routes/skills.js
+++ routes/skills.js
@@ -1,11 +1,15 @@
 import { Router } from 'express'
-
+import * as skillsCtrl from '../controllers/skills.js'
 const router = Router()
 
 /* GET home page. */
 
+// router.get('/', skillsCtrl.index)
+
+
 router.get('/', function(req, res) {
   skillsDb.find({}, function(error, skills) {
+    console.log('SKILLS', skills)
     res.render('skills/index', {
       skills: skills,
       error: error
